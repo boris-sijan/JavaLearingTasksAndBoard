a604a29958cc7494a05bd54d3159b36b42339d0d
Melita Zderic <Melita.Zderic@finnova.com>
2025-12-02 15:51:38 +0100

    Person

---
+++ JavaKurs1/src/Person.java
@@ -0,0 +1,22 @@
+/*
+ * 1. Erstelle ein Programm, welches folgende Werte in den passenden Variablen speichert:
+ *    Startnummer: 1433
+ *    Name: "Alex Jones"
+ *    Startblock: 'a'
+ *    Zielzeit: 12.45
+ * 2. Gib die Werte der Variablen auf der Konsole aus.
+ */
+
+public class Person {
+    public static void main(String[] args) {
+        int startnummer = 1433;
+        String name = "Alex Jones";
+        char startblock = 'a';
+        double zielzeit = 12.45;
+
+        System.out.println("Startnummer: " + startnummer);
+        System.out.println("Name: " + name);
+        System.out.println("Startblock: " + startblock);
+        System.out.println("Zielzeit: " + zielzeit);
+    }
+}
