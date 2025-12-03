f9a1a07206f76d20c6425cf548f5d425e55b5345
Melita Zderic <Melita.Zderic@finnova.com>
2025-12-03 11:44:05 +0100

    Konditionen

---
+++ JavaKurs1/src/Konditionen.java
@@ -0,0 +1,18 @@
+public class Konditionen {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a < b) {
+            System.out.println("a ist kleiner als b");
+        } else if (a > b) {
+            System.out.println("a ist größer als b");
+        } else {
+            System.out.println("a ist gleich b");
+        }
+        //Ternärer Operator
+        String result = (a < b) ? "a ist kleiner als b" : "a ist größer als b";
+        System.out.println(result);
+
+    }
+}
